db8bb88d94ea5b35536b9f29f8de998ea4f34112
Matthew Elliott <matthewe700@gmail.com>
2026-03-15 04:50:24 -0400

    Material Actor
    Pushed Dorian's MaterialActor cpp and header files

---
+++ MaterialActor.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MaterialActor.generated.h"
+
+UCLASS()
+class FINALPROJECTTEST_API AMaterialActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMaterialActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	class UMaterialInterface* BaseMaterial;
+
+	UPROPERTY()
+	class UMaterialInstanceDynamic* DynamicMaterial;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Material Parameters")
+	float Roughness;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Material Parameters")
+	float Metallic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Material Parameters")
+	float EmissiveStrength;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Material Parameters")
+	FLinearColor BaseColor;
+
+	UFUNCTION(BlueprintCallable, Category = "Material")	
+	void UpdateMaterialParameters();
+
+};
